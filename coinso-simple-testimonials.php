5bb0f9c549d58a6bdc7650ac12e790ea201a555f
ido barnea <barbareshet@gmail.com>
2018-10-26 13:16:22 +0300

    1.5.1 update test

--- coinso-simple-testimonials.php
+++ coinso-simple-testimonials.php
@@ -5,7 +5,7 @@
  * Description: A Simple Testimonials Slider
  * Author: Ido @ Coindo
  * Author URI: http://coinso.com/project/ido-barnea
- * Version: 1.5
+ * Version: 1.5.1
  * License: GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: cts
